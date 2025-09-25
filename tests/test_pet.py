21fabf7e9d25a840010154a1b39b968a77c15990
danilcadov <chadov0101@mail.ru>
2025-09-25 19:22:47 +0500

    Написал тесты на "Получение информации о питомце по ID, Обновление информации о питомце, Удаление питомца по ID" с помощью fixture.

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -1,6 +1,6 @@
 import allure
 import jsonschema
-import requests  # Используется для формирования апи запроса
+import requests
 from .schemas.pet_schema import PET_SCHEMA
 
 BASE_URL = "http://5.181.109.28:9090/api/v3"
@@ -57,3 +57,28 @@ class TestPet:
             with allure.step('Проверка статуса ответа и валидации JSON-схемы'):
                 assert responses.status_code == 200, 'Код ответа не совпал с ожидаемым'
                 jsonschema.validate(response.json(), PET_SCHEMA)
+
+    @allure.title('Получение информации о питомце по ID')
+    def test_get_pet_by_id(self, create_pet):
+      with allure.step('Получение ID созданного питомца'):
+          pet_id = create_pet['id']
+
+      with allure.step('Отправка запроса на получение информации о питомце по ID'):
+          response = requests.get(url=f'{BASE_URL}/pet/{pet_id}')
+
+      with allure.step('Проверка статуса ответа и данных питомца'):
+          assert response.status_code == 200
+          assert response.json()['id'] == pet_id
+
+    @allure.title('Обновление информации о питомце')
+    def test_put_pet_info(self, creation_pet, update_pet):
+        with allure.step('Отправление запроса на обновление питомца'):
+          response = requests.post(url=f'{BASE_URL}/pet,{update_pet}')
+
+
+    @allure.title('Удаление питомца по ID')
+    def test_delete_pet(self, create_pet_id, delete_pet):
+        response = requests.get(url=f'{BASE_URL}/pet/{create_pet_id}')
+        assert response.status_code == 404
+
+
