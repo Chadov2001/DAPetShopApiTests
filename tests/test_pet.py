3dc5803891d442c302491248240ecd6f050df9e9
danilcadov <chadov0101@mail.ru>
2025-10-07 19:18:29 +0500

    Написал тест "получение списка питомцев по статусу" с использованием параметризации

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -1,5 +1,6 @@
 import allure
 import jsonschema
+import pytest
 import requests
 from .schemas.pet_schema import PET_SCHEMA
 
@@ -52,11 +53,11 @@ class TestPet:
                     "status": "available"
                 }
             with allure.step('Отправка запроса на создание питомца'):
-                responses = requests.post(url=f'{BASE_URL}/pet', json= first_payload)
+              response = requests.post(url=f'{BASE_URL}/pet', json= first_payload)
 
             with allure.step('Проверка статуса ответа и валидации JSON-схемы'):
-                assert responses.status_code == 200, 'Код ответа не совпал с ожидаемым'
-                jsonschema.validate(response.json(), PET_SCHEMA)
+              assert response.status_code == 200, 'Код ответа не совпал с ожидаемым'
+              jsonschema.validate(response.json(), PET_SCHEMA)
 
     @allure.title('Получение информации о питомце по ID')
     def test_get_pet_by_id(self, create_pet):
@@ -99,3 +100,23 @@ class TestPet:
         with allure.step('Отправляем запрос на получение информации о питомце'):
             response = requests.get(url=f'{BASE_URL}/pet/{pet_id}')
             assert response.status_code == 404
+
+    @allure.title('Получение списка питомцев по статусу')
+    @pytest.mark.parametrize(
+        'status, expected_status_code',
+        [
+        ('available', 200),
+        ('pending', 200),
+        ('sold', 200),
+        ('non_existent', 400),
+        ('', 400)
+        ]
+    )
+    def test_get_pets_by_status(self, status, expected_status_code):
+        with allure.step(f'Отправка запроса на получение питомцев по статусу {status}'):
+          response = requests.get(url=f'{BASE_URL}/pet/findByStatus', params={'status':status})
+
+        with allure.step('Проверка статуса ответа и формата данных'):
+          assert response.status_code == expected_status_code
+          if response.status_code == 200:
+            assert isinstance(response.json(), list)
